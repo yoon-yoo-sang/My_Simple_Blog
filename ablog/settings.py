22287868b11f65058cdef4dafecdba6999a3b8c9
yoon-yoo-sang <yyss6188@naver.com>
2021-01-13 20:57:28 +0900

    host명 지정

--- ablog/settings.py
+++ ablog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'vy7w!(v#imx!zlxl=x^lpm8)faa9=de-5)lih5*rv^*$=4rybq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '.pythonanywhere.com']
 
 
 # Application definition
