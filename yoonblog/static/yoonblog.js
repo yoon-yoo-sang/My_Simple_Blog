92b78c35c302596008ed64a9dc39f0983fb0f927
yoon-yoo-sang <yyss6188@naver.com>
2021-01-13 20:29:54 +0900

    사이드바 고정

--- yoonblog/static/yoonblog.js
+++ yoonblog/static/yoonblog.js
@@ -11,13 +11,16 @@ function menu_flipper(){
     const menuScreen = document.getElementById('menu');
     if(menu_status == true){
         menuScreen.style.width = '400px';
-        menuScreen.style.padding = '30px 12px';
+        menuScreen.style.padding = '0px 12px 30px';
         document.getElementById('menu-open').style.display = 'none';
         document.getElementById('menu-fold').style.display = 'block';
     } else {
         menuScreen.style.width = '0%';
-        menuScreen.style.padding = '30px 0';
+        menuScreen.style.padding = '0 0 30px';
         document.getElementById('menu-open').style.display = 'block';
         document.getElementById('menu-fold').style.display = 'none';
     }
 }
+// function page_down(){
+//     if
+// }
